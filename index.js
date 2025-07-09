ecc94857a03872b6463417b8ad2e845229440630
NullifyCrescent9202 <97800784+NullifyCrescent9202@users.noreply.github.com>
2025-07-09 19:58:01 -0400

    Create book object and array to store book.

---
+++ index.js
@@ -0,0 +1,8 @@
+const userLibrary = [];
+
+function Book(title, author, readStatus, userId){
+    this.title = title;
+    this.author = author;
+    this.readStatus = readStatus;
+    this.userId = userId;
+};
