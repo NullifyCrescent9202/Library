45809332d8325f8758e9e7c51b4a93a064d162e5
NullifyCrescent9202 <97800784+NullifyCrescent9202@users.noreply.github.com>
2025-07-09 20:02:52 -0400

    Add function that creates book and adds it to the user library.

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function Book(title, author, readStatus, userId){
     this.readStatus = readStatus;
     this.userId = userId;
 };
+
+function createBook(title, author, readStatus){
+    book = new Book(title, author, readStatus, crypto.randomUUID());
+    userLibrary.push(book);
+}
