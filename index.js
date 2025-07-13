fc8426eec2b440cf4d9edca89af602424a2e712c
NullifyCrescent9202 <97800784+NullifyCrescent9202@users.noreply.github.com>
2025-07-13 17:17:58 -0400

    Create function that allows user to create and display the book, also change basic styling.

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
 const userLibrary = [];
 
-function Book(title, author, readStatus, userId){
+function Book(title, author, readStatus, Pages, userId){
     this.title = title;
     this.author = author;
     this.readStatus = readStatus;
+    this.Pages = Pages;
     this.userId = userId;
 };
 
-function createBook(title, author, readStatus){
-    book = new Book(title, author, readStatus, crypto.randomUUID());
+function createBook(title, author, Pages, readStatus){
+    let uuid = crypto.randomUUID();
+    book = new Book(title, author, readStatus, Pages, uuid);
     userLibrary.push(book);
+}
+
+
+function createBookElement(title, author, Pages, readStatus, UUID){
+    const body = document.querySelector("body");
+    const card = document.createElement("div");
+
+    card.className = "Card";
+    card.id = UUID;
+    body.appendChild(card);
+
+    const titleHeader = document.createElement("h4");
+    titleHeader.innerText = "Title";
+    card.appendChild(titleHeader);
+
+    const titleContent = document.createElement("div");
+    titleContent.className = "title";
+    titleContent.append(title);
+    card.appendChild(titleContent);
+
+    const authorHeader = document.createElement("h4");
+    authorHeader.innerText = "Author";
+    card.appendChild(authorHeader);
+
+    const authorContent = document.createElement("div");
+    authorContent.className = "author";
+    authorContent.append(author);
+    card.appendChild(authorContent);
+
+    const pageHeader = document.createElement("h4");
+    pageHeader.innerText = "Pages";
+    card.appendChild(pageHeader);
+
+    const pageContent = document.createElement("div");
+    pageContent.className = "pages";
+    pageContent.append(Pages);
+    card.appendChild(pageContent);
+
+    const btns = document.createElement("div");
+    btns.className = "Buttons";
+    card.appendChild(btns);
+
+    statusbtn = document.createElement("button");
+    statusbtn.append("Not Read");
+    statusbtn.id = "status";
+    btns.appendChild(statusbtn);
+
+    deletebtn = document.createElement("button");
+    deletebtn.append("Delete");
+    deletebtn.id = "delete";
+    btns.appendChild(deletebtn);
+    
 }
